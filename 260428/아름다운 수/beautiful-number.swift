89a4db4505bc25d13dfce02847a0025197ea46dc
JiYoungSon <37203919+Chbee@users.noreply.github.com>
2026-04-28 15:50:19 +0900

    Title: 아름다운 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260428/아름다운 수/beautiful-number.swift
+++ 260428/아름다운 수/beautiful-number.swift
@@ -1,24 +1,18 @@
 import Foundation
 
 let n = Int(readLine()!)!
-
 var cnt = 0
-
-func selectNum(rs: [Int]) {
-    if rs.count == n {
-        if rs.contains(1) && rs.filter({ $0 == 1 }).count != 1 { return }
-        if rs.contains(2) && rs.filter({ $0 == 2 }).count != 2 { return }
-        if rs.contains(3) && rs.filter({ $0 == 3 }).count != 3 { return }
-        if rs.contains(4) && rs.filter({ $0 == 4 }).count != 4 { return }
+func choose(num: Int) {
+    if num == n + 1 {
         cnt += 1
         return
     }
-
-    for i in 1...4 {
-        selectNum(rs: rs + [i])
+    if num > n + 1 { return }
+    for select in 1...4 {
+        choose(num: num + select)
     }
 }
 
-selectNum(rs: [])
+choose(num: 1)
 
 print(cnt)
