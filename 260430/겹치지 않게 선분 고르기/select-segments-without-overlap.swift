2d3cb052a5ee96ece1eff0958bf21fb44a57ceb0
JiYoungSon <37203919+Chbee@users.noreply.github.com>
2026-04-30 09:46:06 +0900

    Title: 겹치지 않게 선분 고르기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260430/겹치지 않게 선분 고르기/select-segments-without-overlap.swift
+++ 260430/겹치지 않게 선분 고르기/select-segments-without-overlap.swift
@@ -1,27 +1,49 @@
+struct Segment: Hashable {
+    let a: Int
+    let b: Int
+}
+
 let n = Int(readLine()!)!
-var segments: [(Int, Int)] = []
+var segments: [Segment] = []
 for _ in 0..<n {
     let parts = readLine()!.split(separator: " ").map { Int($0)! }
-    segments.append((parts[0], parts[1]))
+    segments.append(Segment(a: parts[0], b: parts[1]))
 }
 
-var temp = [Int]()
-var rs = [[Int]]()
+func solution() {
+    if n == 1 { print(1); return }
 
-func select(start: Int) {
-    if Set(temp).count == 4 { rs.append(temp); return }
-    for i in start..<n {
-        let (a, b) = segments[i]
-        temp.append(a)
-        temp.append(b)
-        select(start: i + 1)
-        temp.removeLast()
-        temp.removeLast()
+    var temp: [Segment] = []
+    var maxV = 0
+
+    for i in 0..<n {
+        let s = segments[i]
+        temp.append(s)
+        dfs(start: i)
+        temp = []
     }
+
+    func dfs(start: Int) {
+        if start >= n {
+            if Set(temp.flatMap { [$0.a, $0.b] }).count == (temp.count * 2)
+            {
+                maxV = max(maxV, temp.count)
+            }
+            return
+        }
+
+        for i in start..<n {
+            var _temp = temp
+            let current = segments[i]
+            _temp.append(current)
+            guard Set(_temp).count > 1 else { continue }
+            temp.append(current)
+            dfs(start: i + 1)
+            temp.removeLast()
+        }
+    }
+
+    print(maxV)
 }
 
-for i in 0..<n {
-    select(start: i)
-}
-
-print(Set(rs).count)
+solution()
